8e6c5d9804f003c7f346d8e4f7c4e2f2e8a110bc
Shivam8449 <shivamthakur9632@gmail.com>
2026-05-11 15:14:09 +0530

    practice

--- todorev/src/TodoList.jsx
+++ todorev/src/TodoList.jsx
@@ -2,34 +2,32 @@ import React, { useState } from 'react'
 import { useTodos } from './TodoContext'
 
 const TodoList = () => {
-  const {todos,addTodo,toggleTodo,deleteTodo} = useTodos()
-  const [text, setText] = useState('')
+  const {todos,addTodos,toggleTodo,deleteTodo} = useTodos()
+  const[text, setText] = useState('')
 
   const handleAdd = ()=>{
-    addTodo(text)
+    addTodos(text)
     setText('')
   }
   return (
     <div>
       <h1>Todo List</h1>
 
-      <input type="text" 
+      <input type="text"
       value={text}
       onChange={(e)=>setText(e.target.value)}
-      placeholder='Todos...'
+      placeholder='todos...'
       />
 
-      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleAdd} >Add</button>
 
       <ul>
         {todos.map((todo)=>(
-          <li key={todo.id}>
-            <span onClick={()=>toggleTodo(todo.id)} 
-            style={{textDecoration:todo.completed?'line-through':'none', cursor:'pointer'}}
-            >
+          <li key={todo.id} >
+            <span onClick={()=>toggleTodo(todo.id)} style={{textDecoration:todo.completed?'line-through':'none', cursor:'pointer'}} >
               {todo.text}
             </span>
-            <button onClick={()=>deleteTodo(todo.id)} >Delete</button>
+            <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
           </li>
         ))}
       </ul>
